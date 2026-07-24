643b332364ec15f301887879f070a1abb228706e
RdDomingosCuNha <richardsanchez4564@gmail.com>
2026-07-23 22:39:18 -0300

    calculadorasimples.py
    # 🧮 Calculadora Simples em Python
    
    Este é um projeto simples desenvolvido em **Python** para praticar conceitos básicos de programação.
    
    O programa solicita ao usuário dois números e realiza automaticamente quatro operações matemáticas:
    
    * ➕ Soma
    * ➖ Subtração
    * ✖️ Multiplicação
    * ➗ Divisão
    
    ## 📚 Conceitos utilizados
    
    Neste projeto pratiquei:
    
    * Criação de variáveis
    * Entrada de dados com `input()`
    * Conversão de valores utilizando `int()`
    * Operadores matemáticos
    * Exibição de resultados utilizando `print()`
    
    Este projeto faz parte do meu aprendizado em **Python e programação**, sendo um dos meus primeiros projetos publicados no GitHub.

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+n1 = int(input('digite o primeiro numero:'))
+n2 = int(input('digite o segundo numero:'))
+soma = n1 + n2
+subtração = n1 - n2
+multiplicação = n1 * n2
+divisão = n1 / n2
+print('soma', soma)
+print('subtração', subtração)
+print('multiplicação', multiplicação)
+print('divisão', divisão)
